8e36ae34594a8fc582446397ecf68e3bd49a493a
James Fryman <james@fryman.io>
2018-09-05 16:36:22 -0500

    Add links to URL for running execution

--- lib/lita/handlers/rundeck.rb
+++ lib/lita/handlers/rundeck.rb
@@ -268,7 +268,8 @@ module Lita
               "run.still_running",
               id: execution.id,
               current_duration: current_duration,
-              average_duration: average_duration
+              average_duration: average_duration,
+              job_url: execution.href,
               )
             sleep 10
           else
@@ -281,7 +282,7 @@ module Lita
       def resolve(e)
         case e.status
         when "running"
-          t("run.success", id: e.id, average_duration: e.job.average_duration.to_f / 1000.0)
+          t("run.success", id: e.id, average_duration: e.job.average_duration.to_f / 1000.0, job_url: e.href)
         when "api.error.execution.conflict"
           t("run.conflict")
         when "api.error.item.unauthorized"
